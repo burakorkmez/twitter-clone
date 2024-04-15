d52c09d9311a23c4d460ce7972e91eeeeb929118
Burak Orkmez <burakorkmezz@gmail.com>
2024-04-15 23:49:16 +0300

    1- update route files and paths

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
